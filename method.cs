37d53724a0038fe0a45139e2b9e2acc2963600c0
Fidanbaratli1 <fidanbaratli@gmail.com>
2023-03-28 22:27:58 +0400

    done task

---
+++ method.cs
@@ -0,0 +1,26 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 14, 23, 12, 5, 9 };
+            Console.WriteLine(Getmin(arr));
+        }
+    
+            public static int Getmin (int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+                return min;
+            }
+        }
+    }
+   
---
+++ overload.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Area(5);
+            Area(3, 6);
+            Area(4, 6, 9);
+            Area(3,4,6,5);
+        }
+        public static void Area(int r)
+        {
+            int p = 3;
+            int s = p*(r*r);
+            Console.WriteLine(s);    //cevrenin sahesi
+        }
+        public static void Area(int a,int b)
+        {
+            int s = a * b;
+            Console.WriteLine(s);   //Duzbucaqlinin sahesi
+        }
+        public static void Area( int a, int b, int c)
+        {
+            int s = 2 * (a * b + a * c + b * c);
+            Console.WriteLine(s);  //duzbucaqli paralelipipedin tam sethinin sahesi
+        }
+        public static void Area( int a, int b,int c,int r)
+        {
+           int p = (a + b + c) / 2;
+            int s = p * r;
+            Console.WriteLine(s);  // ucbucaqlinin daxiline cekilmis cevrenin sahesi
+        }
+
+    }
+}
